f1c3966597b07a0a80188b301a2c4ab8fec4dc40
jazAlonso <jazminalonsobri@gmail.com>
2025-06-10 09:09:29 -0600

    Implement full drag and drop between lists

--- script.js
+++ script.js
@@ -2,13 +2,44 @@ const cards = document.querySelectorAll(".card")
 const lists = document.querySelectorAll(".list")
 
 for(const card of cards){
-    card.addEventListener("dragstart", dragStart)
-    card.addEventListener("dragend", dragEnd)
+    card.addEventListener("dragstart", dragStart);
+    card.addEventListener("dragend", dragEnd);
 }
 
 for(const list of lists){
-    list.addEventListener("dragover", dragOver)
-    list.addEventListener("dragenter", dragEnter)
-    list.addEventListener("dragleave",dragLeave)
-    list.addEventListener("drop", dragDrop)
+    list.addEventListener("dragover", dragOver);
+    list.addEventListener("dragenter", dragEnter);
+    list.addEventListener("dragleave",dragLeave);
+    list.addEventListener("drop", dragDrop);
+}
+
+function dragStart(e){
+    e.dataTransfer.setData("text/plain", this.id);
+}
+
+function dragEnd(){
+    console.log("Drag ended");
+}
+
+function dragEnter(e){
+    e.preventDefault();
+    this.classList.add("over")
+}
+
+function dragOver(e) {
+    e.preventDefault(); 
+}
+
+function dragLeave(e){
+    e.preventDefault();
+}
+
+function dragDrop(e){
+    const id = e.dataTransfer.getData("text/plain");
+
+    const card = document.getElementById(id);
+
+    this.appendChild(card);
+
+    this.classList.remove("over")
 }

a906254ac4ba168e7bdd9534d7d18bb624640a77
jazAlonso <jazminalonsobri@gmail.com>
2025-06-10 08:57:46 -0600

    Add drag event listeners to cards

---
+++ script.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll(".card")
+const lists = document.querySelectorAll(".list")
+
+for(const card of cards){
+    card.addEventListener("dragstart", dragStart)
+    card.addEventListener("dragend", dragEnd)
+}
+
